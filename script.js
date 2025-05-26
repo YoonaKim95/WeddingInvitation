ed8acbc9027103ed6d507d6357e2be08a8f7fc33
YoonaKim95 <yoonakim95@gmail.com>
2025-05-26 20:53:51 +0900

    kakao test

--- script.js
+++ script.js
@@ -48,3 +48,28 @@
     });
   });
   
+
+  Kakao.init('2f6bea57641d1dd00d85e80a5fb8ba78');
+document.getElementById('kakaotalk-sharing-btn').addEventListener('click', function() {
+  Kakao.Share.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '안병진 💗 김윤아 결혼합니다',
+      description: '2025년 10월 18일 토요일 오후 1시, 서울대학교 연구공원 웨딩홀',
+      imageUrl: 'https://yoonakim95.github.io/WeddingInvitation/thumbnail.png',
+      link: {
+        mobileWebUrl: 'https://yoonakim95.github.io/WeddingInvitation/',
+        webUrl: 'https://yoonakim95.github.io/WeddingInvitation/',
+      },
+    },
+    buttons: [
+      {
+        title: '모바일 청첩장 보기',
+        link: {
+          mobileWebUrl: 'https://yoonakim95.github.io/WeddingInvitation/',
+          webUrl: 'https://yoonakim95.github.io/WeddingInvitation/',
+        },
+      },
+    ],
+  });
+});
