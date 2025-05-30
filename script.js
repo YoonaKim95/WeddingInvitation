e95284d80dff65793c348401d556c512e036acc3
YoonaKim95 <yoonakim95@gmail.com>
2025-05-30 16:33:44 +0900

    LOCATION

--- script.js
+++ script.js
@@ -34,28 +34,6 @@ const countdownInterval = setInterval(function() {
 }, 1000);
 
 
-// const weddingDate = new Date("2025-10-18T13:00:00").getTime();
-// const countdownInterval = setInterval(function() {
-//   const now = new Date().getTime();
-//   const distance = weddingDate - now;
-
-//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   document.getElementById("days").innerText = days;
-//   document.getElementById("hours").innerText = hours;
-//   document.getElementById("minutes").innerText = minutes;
-//   document.getElementById("seconds").innerText = seconds;
-
-//   if (distance < 0) {
-//     clearInterval(countdownInterval);
-//     document.getElementById("timer").innerHTML = "오늘은 결혼식 날입니다!";
-//   }
-// }, 1000);
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const textElement = document.getElementById('typing-text');
   const text = "We're Getting Married!";
